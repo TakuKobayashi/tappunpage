3ffd66b9c68bbdf5d8451bea8457b3ab9dd05a56
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-01-05 14:41:23 +0900

    webpack in vuejs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,36 @@ const path = require('path')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const assetPathRoot = 'public/assets';
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
+  module: {
+  rules: [
+      {
+        test: /\.vue$/, // ファイルが.vueで終われば...
+        loader: 'vue-loader' // vue-loaderを使う
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'] // css-loader -> vue-style-loaderの順で通していく
+      },
+    ]
+  },
+  resolve: {
+    // import './foo.vue' の代わりに import './foo' と書けるようになる(拡張子省略)
+    extensions: ['.js', '.vue'],
+    alias: {
+      // vue-template-compilerに読ませてコンパイルするために必要
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   entry: {
     index: path.resolve(__dirname, 'frontend/javascripts/index.js')
   },
@@ -14,6 +43,6 @@ module.exports = {
   },
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' },
-    contentBase: assetPathRoot,
+    contentBase: path.resolve(__dirname, assetPathRoot),
   },
 }
