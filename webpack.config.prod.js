73a9cb6c8747ba676e285aedd3f25b51e0f7b0eb
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-01-08 02:06:04 +0900

    buildコマンドを変えてみた

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -21,13 +21,9 @@ var styleSheetRootPath = path.resolve(__dirname, 'frontend/stylesheets');
 var imageRootPath = path.resolve(__dirname, 'frontend/images');
 
 var entries = {}
-var jsPathes = glob.sync(path.resolve(__dirname, javascriptRootPath + '/*.js'));
-var cssPathes = glob.sync(path.resolve(__dirname, javascriptRootPath + '/*.{css,scss}'));
-var resourcePathes = glob.sync(path.resolve(__dirname, javascriptRootPath + '/*.{gif,png,jpg,eot,wof,woff,woff2,ttf,svg}'));
-
 var fileNames = []
 fileNames = fileNames.concat(glob.sync(path.resolve(__dirname, javascriptRootPath + '/*.js')));
-fileNames = fileNames.concat(glob.sync(path.resolve(__dirname, styleSheetRootPath + '/*.{css,scss}')));
+fileNames = fileNames.concat(glob.sync(path.resolve(__dirname, styleSheetRootPath + '/*.css')));
 fileNames = fileNames.concat(glob.sync(path.resolve(__dirname, imageRootPath + '/*.{gif,png,jpg,eot,wof,woff,woff2,ttf,svg}')));
 fileNames.forEach(function(fileName){
   var rootPath = fileName.replace(javascriptRootPath + "/", "").replace(styleSheetRootPath + "/", "").replace(imageRootPath + "/", "");
@@ -35,66 +31,37 @@ fileNames.forEach(function(fileName){
 });
 console.log(entries);
 
-const loaders = [
-  {
-    test: /\.js$/,
-    use: [
-      {
-        // Babel を利用する
-        loader: 'babel-loader',
-        // Babel のオプションを指定する
-      }
-    ]
-  },
-  // Sassファイルの読み込みとコンパイル
-  {
-    test: /\.(scss|css)$/, // 対象となるファイルの拡張子
-    // ローダー名
-    use: [
-      // linkタグに出力する機能
-      'style-loader',
-      // CSSをバンドルするための機能
-      {
-        loader: 'css-loader',
-        options: {
-          // オプションでCSS内のurl()メソッドを取り込む
-          url: true,
-          // ソースマップの利用有無
-          sourceMap: false,
-         // 0 => no loaders (default);
-          // 1 => postcss-loader;
-          // 2 => postcss-loader, sass-loader
-          importLoaders: 2
-        },
-      },
-      // Sassをバンドルするための機能
-      {
-        loader: 'sass-loader',
-        options: {
-          // ソースマップの利用有無
-          sourceMap: false,
-        }
-      }
-    ],
-  },
-  {
-    // 対象となるファイルの拡張子
-    test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
-    // 画像をBase64として取り込む
-    loader: 'url-loader'
-  }
-];
-
 const assetPathRoot = 'public/assets';
 
 module.exports = {
   entry: entries,
   output: {
-    path: path.resolve(__dirname, assetPathRoot),
+    path: path.resolve(__dirname, 'public/assets'),
     filename: '[name]-[hash].js'
   },
   plugins: plugins,
   module: {
-    rules: loaders
+    rules: [
+      {
+        test: /\.vue$/, // ファイルが.vueで終われば...
+        loader: 'vue-loader' // vue-loaderを使う
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+        ] // css-loader -> vue-style-loaderの順で通していく
+      },
+      {
+        test: /\.(gif|png|jpe?g|otf|eot|wof|woff|woff2|ttf|svg)(\?.+)?$/,
+        loader: 'url-loader'
+      },
+    ]
   }
 }
